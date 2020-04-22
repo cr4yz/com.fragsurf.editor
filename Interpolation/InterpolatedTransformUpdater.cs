d9d2276269bb69ed00adedf12ae3bfa58bfbeccf
Crayz <jacob.ee92@gmail.com>
2020-04-21 20:57:35 -0500

    Initial commit

---
+++ Interpolation/InterpolatedTransformUpdater.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InterpolatedTransformUpdater : MonoBehaviour
+{
+    private InterpolatedTransform m_interpolatedTransform;
+
+    void Awake()
+    {
+        m_interpolatedTransform = GetComponent<InterpolatedTransform>();
+    }
+
+    void FixedUpdate()
+    {
+        m_interpolatedTransform.LateFixedUpdate();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# com.fragsurf.editor
+ 
